c56f459cbedbe6ece2f1d5b43fc62bc46a8fa6ae
Daniel Jackson <d.jackson.contact@gmail.com>
2026-04-24 15:03:32 +0100

    feat: the initial HTML, CSS and JavaScript for the accordion component added

---
+++ src/scripts/accordion.js
@@ -0,0 +1,8 @@
+    const buttons = document.querySelectorAll(".accordion-button");
+
+    buttons.forEach(button => {
+      button.addEventListener("click", () => {
+        const content = button.nextElementSibling;
+        content.classList.toggle("open");
+      });
+    });
