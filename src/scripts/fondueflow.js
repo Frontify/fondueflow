e85c4f00fdf41df72c93b33071f611741bf5df06
Daniel Jackson <d.jackson.contact@gmail.com>
2026-05-07 09:58:19 +0100

    fix: add a variable to identify the version of the framework currently being used

--- src/scripts/fondueflow.js
+++ src/scripts/fondueflow.js
@@ -1,2 +1,8 @@
 import "./components/accordion.js";
 import "./components/navbar.js";
+
+/*
+ * Use the variable to check the version of the framework currently being used.
+ * This can be output in a console.log: console.log(FONDUE_VERSION)
+ */
+const FONDUE_VERSION = "0.3.2";
