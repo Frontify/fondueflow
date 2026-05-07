d9d78174df4c7944f9ee1bf81f46ced48b4eeaf9
Daniel Jackson <d.jackson.contact@gmail.com>
2026-05-07 10:24:19 +0100

    fix: attach the FONDUE_VERSION variable to the window object to be output in the console

--- src/scripts/fondueflow.js
+++ src/scripts/fondueflow.js
@@ -2,7 +2,10 @@ import "./components/accordion.js";
 import "./components/navbar.js";
 
 /*
- * Use the variable to check the version of the framework currently being used.
- * This can be output in a console.log: console.log(FONDUE_VERSION)
+ * Use this variable to check the version of the framework currently being used.
+ * This can be output in this module with: 
+ * console.log(FONDUE_VERSION)
  */
 const FONDUE_VERSION = "0.3.2";
+// Attach variable to the window object to allow output in the console.
+window.FONDUE_VERSION = FONDUE_VERSION;
