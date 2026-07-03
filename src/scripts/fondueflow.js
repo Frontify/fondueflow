c4791fce88eb06243a8511d7da5cdeef857cf6be
Daniel Jackson <d.jackson.contact@gmail.com>
2026-07-03 10:07:58 +0100

    fix: remove the reset.css from being included in the bundle as this was breaking the scroll within the Webflow design canvas

--- src/scripts/fondueflow.js
+++ src/scripts/fondueflow.js
@@ -17,7 +17,7 @@ import { initNavbar } from "./components/navbar.js";
  * This can be output in this module with: 
  * console.log(FONDUE_VERSION)
  */
-const FONDUE_VERSION = "0.4.0";
+const FONDUE_VERSION = "0.4.1";
 // Attach variable to the window object to allow output in the console.
 window.FONDUE_VERSION = FONDUE_VERSION;
 
